a68e2249769f8ac9842aa9457ba56c0ef90a48eb
Lucas Leandro <lucasjleandro@gmail.com>
2022-02-15 23:19:09 -0300

    Concluindo despesas

--- tests/receita.test.js
+++ tests/receita.test.js
@@ -1,9 +1,6 @@
 const request = require("supertest");
 const app = require('../api/app');
 
-// import request from 'supertest'
-// import app from '../api/app'
-
 describe('Testando os Endpoints de Receita', () => {
     it('A Listagem de Receitas deve acontecer com sucesso.', async () => {
         const res = await request(app).get('/api/orcamento/receitas');
@@ -17,6 +14,12 @@ describe('Testando os Endpoints de Receita', () => {
         expect(200).toEqual(res.statusCode);
     });
 
+    it('Obter Receita por Mês e Ano com sucesso.', async () => {
+        const res = await request(app).get('/api/orcamento/receitas/2022/1');
+
+        expect(200).toEqual(res.statusCode);
+    });
+
     it('Cadastrar Receita com sucesso.', async() => {
         const res = await request(app).post('/api/orcamento/receitas').send({
             "descricao": "Salario Mensal",
@@ -25,6 +28,23 @@ describe('Testando os Endpoints de Receita', () => {
         })
 
         console.log(`Recebido ${res}`);
+        expect(200).toEqual(res.statusCode);
+    });
+
+    it('Alterar Receita com sucesso.', async() => {
+        const res = await request(app).put('/api/orcamento/receitas/1').send({
+            "descricao": "Salario Mensal",
+            "valor": 3500,
+            "data": "2022/01/20"
+        })
+
+        console.log(`Recebido ${res}`);
+        expect(200).toEqual(res.statusCode);
+    });
+
+    it('Remover Receita com sucesso.', async() => {
+        const res = await request(app).get('/api/orcamento/receita/1');
+
         expect(200).toEqual(res.statusCode);
     });
 });
